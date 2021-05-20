f2e979993e39b7cd9485e3bd4d940bb492d01d1f
braunsteinbryan <braunsteinbryan@gmail.com>
2021-05-20 15:12:13 -0400

    added memoized canSum function

--- canSum.js
+++ canSum.js
@@ -32,3 +32,37 @@ console.log(canSum(7, [5, 3, 4, 7])); // true
 console.log(canSum(7, [2, 4])); // false
 console.log(canSum(8, [2, 3, 5])); // true
 console.log(canSum(300, [7, 14])); // false, however, our previous code lacks the efficiency to solve this quickly
+
+
+// Here we have the same function again, but memoized
+
+const canSum = (targetSum, numbers, memo={}) => {
+    if (targetSum in memo) {
+        return memo[targetSum]
+    };
+    if (targetSum === 0) {
+        return true
+    };
+    if (targetSum < 0) {
+        return false
+    };
+
+    for (let num of numbers) {
+        const remainder = targetSum - num;
+        if (canSum(remainder, numbers, memo) === true) {
+            memo[targetSum] = true;
+            return true;
+        }
+    }
+
+    memo[targetSum] = false;
+    return false;
+};
+
+
+
+console.log(canSum(7, [2, 3])) // true
+console.log(canSum(7, [5, 3, 4, 7])); // true
+console.log(canSum(7, [2, 4])); // false
+console.log(canSum(8, [2, 3, 5])); // true
+console.log(canSum(300, [7, 14])); // false
