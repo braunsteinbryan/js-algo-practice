52a408719f3595e8d8cc99344def0e0c70eb85ef
braunsteinbryan <braunsteinbryan@gmail.com>
2021-05-24 09:06:08 -0400

    added memoized howSum problem and bestSum problem

---
+++ bestSum.js
@@ -0,0 +1,37 @@
+// Write a function 'bestSum(targetSum, numbers)' that takes in a targetSum and an array of numbers as arguments.
+
+// The function should return an array containing the shortest combonation of numbers that add up to exactly the targetSum.
+
+// If there is a tie for the shortest combination, you may return any of the shortest.
+
+
+const bestSum = (targetSum, numbers) => {
+    if (targetSum === 0) {
+        return []
+    };
+    if (targetSum < 0) {
+        return null
+    };
+
+    let shortestCombination = null;
+
+    for (let num of numbers) {
+        const remainder = targetSum - num;
+        const remainderCombination = bestSum(remainder, numbers);
+        if (remainderCombination !== null) {
+            const combination = [ ...remainderCombination, num];
+            // if the combination is shoter than the current "shortest", update it
+            if (shortestCombination === null || combination.length < shortestCombination.length) {
+                shortestCombination = combination
+            };
+        }
+    }
+
+    return shortestCombination;
+};
+
+
+
+console.log(bestSum(7, [5, 3, 4, 7])); // [7]
+console.log(bestSum(8, [2, 3, 5])); // [3, 5]
+console.log(bestSum(8, [1, 4, 5])); // [4, 4]
